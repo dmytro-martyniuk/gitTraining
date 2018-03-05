76635d7181284f0728396eef1a85d98ac354ffd2
Konstantin Kuzmin <kocc95@mail.ru>
2018-03-05 21:06:09 +0100

    add operation test is implemented

--- src/test/java/CalculatorImplTest.java
+++ src/test/java/CalculatorImplTest.java
@@ -1,8 +1,18 @@
 import org.junit.Test;
 
-public class CalculatorImplTest {
-    @Test
-    public void test() {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
 
+public class CalculatorImplTest {
+
+    private Calculator calc;
+
+    @Test
+    public void addTest() {
+        calc = new CalculatorImpl();
+
+        int suma = calc.suma(5, 3);
+
+        assertThat(suma, equalTo(8));
     }
 }
